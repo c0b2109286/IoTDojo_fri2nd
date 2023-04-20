b5989861fbd3a6a9d5d975de34ea2ee662d69da2
hikapipo <c0b2107561@edu.teu.ac.jp>
2023-04-20 13:04:52 +0900

    BLE_code

--- BLE/adv.py
+++ BLE/adv.py
@@ -1,4 +1,4 @@
-import ubluetooth
+import bluetooth
 import random
 import struct
 import time
@@ -10,9 +10,8 @@ _IRQ_CENTRAL_CONNECT                 = const(1)
 _IRQ_CENTRAL_DISCONNECT              = const(2)
 
 # デバイス情報サービス
-_Dev_Info_UUID = ubluetooth.UUID(0x180A)
-# デバイス名
-_Dev_CHAR = (ubluetooth.UUID(0x2A7D), ubluetooth.FLAG_READ|ubluetooth.FLAG_WRITE,)
+_Dev_Info_UUID = bluetooth.UUID(0x180A)
+_Dev_CHAR = (bluetooth.UUID(0x2A00), bluetooth.FLAG_READ|bluetooth.FLAG_WRITE,)
 _Dev_SERVICE = (_Dev_Info_UUID, (_Dev_CHAR,),)
 
 class BLE:
@@ -20,18 +19,17 @@ class BLE:
     ble = None
     name = None
 
-    def __init__(self, ble,name):
+    def __init__(self, ble, name):
         self._ble = ble
         self._ble.active(True)
         self._ble.irq(self._irq)
-        ((self._handle,),) = self._ble.gatts_register_services((_Dev_Info_UUID,))
+        ((self._handle,),) = self._ble.gatts_register_services((_Dev_SERVICE,))
         self._connections = set()
         self._payload = advertising_payload(
             name=name, services=[_Dev_Info_UUID], appearance=0)
         self._advertise()
 
     def _irq(self, event, data):
-        # Track connections so we can send notifications.
         if event == _IRQ_CENTRAL_CONNECT:
             conn_handle, _, _ = data
             self._connections.add(conn_handle) #要素追加
@@ -39,11 +37,9 @@ class BLE:
             conn_handle, _, _ = data
             self._connections.remove(conn_handle) #要素削除
             # Start advertising again to allow a new connection.
-            self._advertise() #advertiseに戻る
+            self._advertise()
 
     def set_dev_name(self, dev_name, notify=False, indicate=False):
-        # Data is sint16 in degrees Celsius with a resolution of 0.01 degCelsius.
-        # Write the local value, ready for a central to read.
         self._ble.gatts_write(self._handle, dev_name) #ハンドルのローカル値を書き込む
         if notify: #flag書き換え必須
             for conn_handle in self._connections:
@@ -55,8 +51,8 @@ class BLE:
 
 
 def BLE_adv():
-    ble = ubluetooth.BLE
-    name = ble.config(gap_name='senser01')
+    ble = bluetooth.BLE
+    name = ble.config(gap_name='s01')
     name_enc = name.encode('utf-8') #文字コード(16進数)へ変換
     dev_name = binascii.unhexlify(name_enc) #byte型(バイナリデータ)へ変換
     b = BLE(ble, dev_name)
