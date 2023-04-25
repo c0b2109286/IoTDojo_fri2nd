eb88a0ed4df3d3122c4463e5440ca0a4651e60bb
c0b2109286 <85670850+c0b2109286@users.noreply.github.com>
2023-04-25 13:40:20 +0900

    route変更

--- flask_a/app.py
+++ flask_a/app.py
@@ -15,14 +15,19 @@ class Post(db.Model):
 
 line = [(9,10),(9,7),(10,7),(10,11),(11,8),(11,7),(7,3),(7,5),(8,3),(5,3),(8,4),(4,1),(3,1),(2,1),(5,2),(7,8)  ,(6,5),(6,3),(6,7),(6,10),(6,9)]
 
-@app.route('/admin')
+@app.route('/admin', methods = ["GET", "POST"])
 def route():
-    posts = Post.query.all()
-    return render_template('index2.html', posts = posts)
+    if request.method == 'GET':
+        location = "ALL"
+        return render_template('index2.html', location = location)
+    
+    else:
+        location = request.form.get("btn", None)
+        print(location)
+        return render_template('index2.html', location = location)
 
-@app.route('/map',methods = ["GET", "POST"])
-def foliummap():
-    global First_round
+@app.route('/map/<location>',methods = ["GET"])
+def foliummap(location):
     start_cords=(35.67061628919986, 139.69567437962016)
     folium_map = folium.Map(location=start_cords, zoom_start=17)
 
@@ -32,22 +37,12 @@ def foliummap():
     for post in posts:
         gps_group.append(post.gps.split(","))
 
-    if request.method == 'GET':
-        print("GET")
-        return render_template('index.html')
+    if location == "ゴミ箱A":
+        for loc in line:
+            folium.PolyLine(locations = [np.float_(gps_group[loc[0]-1]), np.float_(gps_group[loc[1]-1])], color = "gray").add_to(folium_map)
 
-    
-    else:
-        print("POST")
-        value = request.form.get("btn", None)
-        if value == "ALL":
-            for loc in line:
-                folium.PolyLine(locations = [np.float_(gps_group[loc[0]-1]), np.float_(gps_group[loc[1]-1])], color = "gray").add_to(folium_map)
-        elif value == "ログアウト":
-            return redirect("/map")
-
-        folium_map.save('templates/index.html')
-        return redirect("/admin")
+    folium_map.save('templates/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
