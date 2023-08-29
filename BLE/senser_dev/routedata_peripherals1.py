4a91dbddc14791b6e1a9c19637713c5931e0b71b
c0b21075 <85730292+c0b2107561@users.noreply.github.com>
2023-08-29 13:55:05 +0900

    Update routedata_peripherals1.py

--- BLE/senser_dev/routedata_peripherals1.py
+++ BLE/senser_dev/routedata_peripherals1.py
@@ -1,8 +1,3 @@
-# This example demonstrates a simple temperature sensor peripheral.
-#
-# The sensor's local value updates every second, and it will notify
-# any connected central every 10 seconds.
-
 import bluetooth
 import random
 import struct
@@ -25,7 +20,7 @@ _FLAG_INDICATE = const(0x0020)
 _Dev_Info_UUID = bluetooth.UUID(0x180A)
 # デバイスの名前
 _Dev_CHAR = (bluetooth.UUID(0x2A00),
-    _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,) #読、通知、応答要求付き通知
+    _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,) # 読み取り，通知，応答要求付き通知
 _Dev_SERVICE = (_Dev_Info_UUID,(_Dev_CHAR,),)
 
 class BLE:
@@ -44,18 +39,17 @@ class BLE:
     def _payload_1(self, name):
         self._name = name
         self._payload_1 = advertising_payload(
-            name=name, services=[_Dev_Info_UUID], appearance= 0)
+            name=name, services=[_Dev_Info_UUID], appearance=0)
         self._advertise()
         
     def _payload_2(self, name):
         self._name = name
         self._payload_2 = advertising_payload(
-            name=name, services=[_Dev_Info_UUID], appearance= 0)
+            name=name, services=[_Dev_Info_UUID], appearance=0)
         self._stop()
 
-
     def _irq(self, event, data):
-        # Track connections so we can send notifications.
+        # 接続を追跡して通知を送信できるようにする．
         if event == _IRQ_CENTRAL_CONNECT:
             conn_handle, _, _ = data
             self._connections.add(conn_handle)
@@ -63,38 +57,34 @@ class BLE:
         elif event == _IRQ_CENTRAL_DISCONNECT:
             conn_handle, _, _ = data
             self._connections.remove(conn_handle)
-            # Start advertising again to allow a new connection.
+            # 新しい接続を許可するために再びアドバタイズを開始する．
             self._check = False
             self._advertise()
         elif event == _IRQ_GATTS_INDICATE_DONE:
             conn_handle, value_handle, status = data
 
     def set_dev_name(self, name, notify=False, indicate=False):
-        # Data is sint16 in degrees Celsius with a resolution of 0.01 degrees Celsius.
-        # Write the local value, ready for a central to read.
         fm = '{}si'.format(len(name))
-        self._ble.gatts_write(self._handle, struct.pack(fm,name)) #読み込み可能な書き込み
-        #self._ble.gatts_write(self._handle, name)
+        self._ble.gatts_write(self._handle, struct.pack(fm, name))  # 読み込み可能な書き込み
         if notify or indicate:
             for conn_handle in self._connections:
                 if notify:
-                    # Notify connected centrals.
+                    # 接続されたセントラルに通知する．
                     self._ble.gatts_notify(conn_handle, self._handle)
                 if indicate:
-                    # Indicate connected centrals.
+                    # 接続されたセントラルに示す．
                     self._ble.gatts_indicate(conn_handle, self._handle)
 
     def _advertise(self, interval_us=100000):
         self._ble.gap_advertise(interval_us, adv_data=self._payload_1)
 
-    def _stop(self, interval_us = None):
+    def _stop(self, interval_us=None):
         self._ble.gap_advertise(interval_us, adv_data=self._payload_2)
-
 
 def periph(timeout=10):
     ble = bluetooth.BLE()
     gapname = manegement_s1.nameinfo()
-    ble.config(gap_name= str(gapname))
+    ble.config(gap_name=str(gapname))
     set_name = ble.config('gap_name')
     print(set_name)
     
@@ -117,13 +107,10 @@ def periph(timeout=10):
             utime.sleep_ms(1000)
             timeout -=1
             
-    if timeout is 0:
+    if timeout == 0:
         b._payload_2("com8")
         b.set_dev_name(data, notify=i == 0, indicate=False)
         print("終了")
         
-    
-
-
 if __name__ == "__main__":
     periph()
