e9e670686ad2ea6251724d8e7b06af219b726c56
c0b21075 <85730292+c0b2107561@users.noreply.github.com>
2023-08-29 13:42:55 +0900

    Update manegement_s1.py

--- BLE/senser_dev/manegement_s1.py
+++ BLE/senser_dev/manegement_s1.py
@@ -5,25 +5,27 @@ import makeroute_s1
 import get_s1
 
 def nameinfo():
-    dev_name = 8
+    dev_name = 8 # デバイス名
     return dev_name
 def packetinfo():
-    dev_packet = "esp32-1A"
+    dev_packet = "esp32-1A" # パケット名
     return dev_packet
 def positioninfo():
-    dev_position = "senser01"
+    dev_position = "senser01" # 用途
     return dev_position
 
 class Management():
-        
-    def _RoutedataSend(self):
+    # センサデバイスの行う動作を纏めたクラス.
+    # Manegmentクラスを定義して経路データの送受信と書き込み(，確認)を行う各モジュールを呼び出す関数を定義する．
+    
+    def _RoutedataSend(self): # 経路データの送信
         routedata_peripherals1.periph()
         
-    def _RoutedataGet(self):
+    def _RoutedataGet(self): # 経路表作成用のデータ取得
         routedata = routeget_centrals1.Centr()
         return routedata
         
-    def _RoutedataWrite(self,route):
+    def _RoutedataWrite(self,route): # 経路表作成用データの保存
         with open('data/makeroute_data.txt','w',encoding='utf-8')as f:
             print(route)
             print(type(route))
@@ -35,13 +37,13 @@ class Management():
             print(f.read())
         f.close()
 
-    def _MakeRouteTable(self):
+    def _MakeRouteTable(self): # 経路表作成
         #with open("data/makeroute_data.txt",'r',encoding="utf-8")as f:
         #    f.read()
         #f.close()
         makeroute_s1._routemake()
         
-    def getdistance(self):
+    def getdistance(self): # 距離データの取得
         distance = get_s1.distance()
         print("#####")
         print(distance) #ここで絶対にstrにしておく
@@ -50,7 +52,7 @@ class Management():
         print(type(distance))
         return distance
         
-    def SenserdataSend(self,distance):
+    def SenserdataSend(self,distance): # 作成した経路表に基づく距離データの送信
         senddistance_peripherals1.periph(distance,10)
 
 if __name__ == "__main__":
