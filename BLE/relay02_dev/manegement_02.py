e7bbc4c3f4393c2dd0332121296c659280092bf0
c0b21075 <85730292+c0b2107561@users.noreply.github.com>
2023-09-15 12:18:34 +0900

    Update manegement_02.py

--- BLE/relay02_dev/manegement_02.py
+++ BLE/relay02_dev/manegement_02.py
@@ -1,70 +1,95 @@
-import routedata_sendtoserver02
-import routedata_central02
-import senddistance_central02
-import distance_sendtoserver02
+import routedata_sendtoserver2
+import routedata_central2
+import senddistance_central2
+import distance_sendtoserver2
 #import senddistance_peripheral
 #import makeroute
 import ubinascii
-
-def nameinfo():
-    dev_name = 5
-    return dev_name
-def packetinfo():
-    dev_packet = "esp32-3A"
-    return dev_packet
-def positioninfo():
-    dev_position = "relay12"
-    return dev_position
+import json
+import routedata_getfromserver1
+import routesendtodevice_peripheral1
 
 class Management():
     
     def _RoutedataGet(self):
-        get = routedata_central02.Centr()
+        get = routedata_central2.Centr()
         return get
         
     def _RoutedataSendtoServer(self,senddata):
-        routedata_sendtoserver02.send(senddata)
+        routedata_sendtoserver2.send(senddata)
     
-    def _check(self):
-        with open("data/senddata2.txt",'r')as f:
-            print(f.readline())
-        f.close()
-
-    def _RoutedataCatch(self):
-        data = central.Centr()
-        print(data)
-        return data
+    #実際に受け取るときには必要
+    def _RouteGetFromServer(self):
+        GS = routedata_getfromserver1.get()
+        return GS
+        
+    def _RoutedataWrite(self, data):
+        with open('data/routeinfo.txt','w', encoding="utf-8")as f:
+            print(data)
+            #print(type(data))
+            f.write(data)
+            f.close
+            
+    def _SendRouteForDevice(self,data):
+        routesendtodevice_peripheral1.periph(data)
+        
 
     def _MakeRouteTable(self,data):
-        with open("routedata_test.txt",'w',encoding="utf-8")as f:
-            f.write(data)
-        f.close()
+        #with open("routedata_test.txt",'w',encoding="utf-8")as f:
+        #    f.writelines(data)
+        #f.close()
         makeroute._routemake()
         
     def SenserdataGet(self):
-        distance = senddistance_central02.Centr()
+        distance = senddistance_central2.Centr()
         return distance
         
     def SenserdataSend(self,distance):
-        distance_sendtoserver02.send(distance)
+        distance_sendtoserver2.send(distance)
 
 if __name__ == "__main__":
-    print(ubinascii.hexlify('com5'))
+    print(ubinascii.hexlify('esp32'))
     mg = Management()
-    #routedata = mg._RoutedataGet()
-    #senddata = routedata + '_' + str(nameinfo())
     
-    #mg._RoutedataSendtoServer(senddata)
-    #mg._check()
+    routedata = mg._RoutedataGet()
+    
+    jf_open = open('info/DN02.json', 'r')
+    jf_load = json.load(jf_open)
+    gapname = jf_load["device_number"]
+
+    route = routedata + '_' + str(gapname)
+    print(route)
+    print(len(route))
+    if len(route) is 3:
+        hop = len(route) - 2
+    if len(route) is 5:
+        hop = len(route) - 3
+    if len(route) is 7:
+        hop = len(route) -4
+    if len(route) is 9:
+        hop = len(route) -5
+
+    senddata = route + '_' + str(hop)
+    print(senddata)
+    
+    response = mg._RoutedataSendtoServer(senddata)
+    
+    getroutedata = mg._RouteGetFromServer()
+    
+    #getroutedata = getroutedata.replace(',','\n')
+    
+    mg._RoutedataWrite(getroutedata)
+    
+    mg._SendRouteForDevice(getroutedata)
     
     #route_data = mg._RoutedataCatch()
     #mg._MakeRouteTable()
+    
     distance = mg.SenserdataGet()
     
     #dist = distance.replace(" ",'')
-    dist = distance.replace('\x00','')
-    print(dist)
-    print(type(dist))
+    #dist = distance.replace('\x00','')
+    #print(dist)
+    #print(type(dist))
     
-    mg.SenserdataSend(dist)
-
+    mg.SenserdataSend(distance)
