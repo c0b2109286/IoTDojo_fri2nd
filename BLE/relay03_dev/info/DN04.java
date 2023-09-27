6debc88ee2c8491b0665e3f8d31d970b221b50fe
c0b21075 <85730292+c0b2107561@users.noreply.github.com>
2023-09-27 11:13:47 +0900

    Update DN04.java

--- BLE/relay03_dev/info/DN04.java
+++ BLE/relay03_dev/info/DN04.java
@@ -1,4 +1,7 @@
 {
-  "device_number" : "4",
-  "packet_name" : "esp32_relay4"
-}
+    "device_number" : "4",
+    "packet_name" : "esp32_relay4",
+    "packet_routeTS" : "relay4_toserver",
+    "packet_routeTD" : "relay4_todevice"
+  }
+  
