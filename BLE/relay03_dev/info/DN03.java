3f765e719cf300a1e933d14b90b056931178f686
c0b21075 <85730292+c0b2107561@users.noreply.github.com>
2023-09-27 11:12:51 +0900

    Update DN03.java

--- BLE/relay03_dev/info/DN03.java
+++ BLE/relay03_dev/info/DN03.java
@@ -1,4 +1,7 @@
 {
-  "device_number" : "3",
-  "packet_name" : "esp32_relay3"
-}
+    "device_number" : "3",
+    "packet_name" : "esp32_relay3",
+    "packet_routeTS" : "relay3_toserver",
+    "packet_routeTD" : "relay3_todevice"
+  }
+  
