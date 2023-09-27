3823b9669d4904b7fba5af0eb5735b7622c3913d
c0b21075 <85730292+c0b2107561@users.noreply.github.com>
2023-09-27 11:59:25 +0900

    Update and rename routedata_peripheral01.py to routedata_peripheral.py

--- BLE/relay01_dev/routedata_peripheral.py
+++ BLE/relay01_dev/routedata_peripheral.py
@@ -1,21 +1,18 @@
-# This example demonstrates a simple temperature sensor peripheral.
-#
-# The sensor's local value updates every second, and it will notify
-# any connected central every 10 seconds.
-
 import bluetooth
 import random
 import struct
-import time
+import utime
 import binascii
 from BLE_advertising import advertising_payload
-# import manegement_01
-import info
+# import manegement
+import info 
+import json
 
 from micropython import const
 
 _IRQ_CENTRAL_CONNECT = const(1)
 _IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_PERIPHERAL_DISCONNECT = const(8)
 _IRQ_GATTS_INDICATE_DONE = const(20)
 
 _FLAG_READ = const(0x0002)
@@ -26,7 +23,7 @@ _FLAG_INDICATE = const(0x0020)
 _Dev_Info_UUID = bluetooth.UUID(0x180A)
 # デバイスの名前
 _Dev_CHAR = (bluetooth.UUID(0x2A00),
-    _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,) #読、通知、応答要求付き通知
+    _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,) # 読み取り，通知，応答要求付き通知
 _Dev_SERVICE = (_Dev_Info_UUID,(_Dev_CHAR,),)
 
 class BLE:
@@ -41,111 +38,113 @@ class BLE:
         ((self._handle,),) = self._ble.gatts_register_services((_Dev_SERVICE,))
         self._connections = set()
         self._check = False
+        self._connect_count = 0
 
     def _payload_1(self, name):
         self._name = name
         self._payload_1 = advertising_payload(
-            name=name, services=[_Dev_Info_UUID], appearance= 0)
+            name=name, services=[_Dev_Info_UUID], appearance=0)
         self._advertise()
         
     def _payload_2(self, name):
         self._name = name
+        self._payload_1 = advertising_payload(
+            name=name, services=[_Dev_Info_UUID], appearance=0)
+        self._advertise()
+        
+    def _payload_3(self, name):
+        self._name = name
         self._payload_2 = advertising_payload(
-            name=name, services=[_Dev_Info_UUID], appearance= 0)
+            name=name, services=[_Dev_Info_UUID], appearance=0)
         self._stop()
 
-
     def _irq(self, event, data):
-        # Track connections so we can send notifications.
+        # 接続を追跡して通知を送信できるようにする．
         if event == _IRQ_CENTRAL_CONNECT:
             conn_handle, _, _ = data
             self._connections.add(conn_handle)
             self._check = True
+            print(self._check)
+            self._connect_count += 1
+            print(f"connection : {self._connect_count}")
+            utime.sleep_ms(3000)
+            self._check = False
         elif event == _IRQ_CENTRAL_DISCONNECT:
             conn_handle, _, _ = data
             self._connections.remove(conn_handle)
-            # Start advertising again to allow a new connection.
+            # 新しい接続を許可するために再びアドバタイズを開始する．
             self._check = False
             self._advertise()
+
+        elif event == _IRQ_PERIPHERAL_DISCONNECT:
+            conn_handle, _, _ = data
+            self._connections.remove(conn_handle)
+            # 新しい接続を許可するために再びアドバタイズを開始する．
+            self._check = False
+            self._advertise()
+
         elif event == _IRQ_GATTS_INDICATE_DONE:
             conn_handle, value_handle, status = data
+            print(data)
 
     def set_dev_name(self, name, notify=False, indicate=False):
-        # Data is sint16 in degrees Celsius with a resolution of 0.01 degrees Celsius.
-        # Write the local value, ready for a central to read.
         fm = '{}si'.format(len(name))
-        self._ble.gatts_write(self._handle, struct.pack(fm,name)) #読み込み可能な書き込み
-        #self._ble.gatts_write(self._handle, name)
+        self._ble.gatts_write(self._handle, struct.pack(fm, name))  # 読み込み可能な書き込み
         if notify or indicate:
             for conn_handle in self._connections:
                 if notify:
-                    # Notify connected centrals.
+                    # 接続されたセントラルに通知する．
                     self._ble.gatts_notify(conn_handle, self._handle)
                 if indicate:
-                    # Indicate connected centrals.
+                    # 接続されたセントラルに示す．
                     self._ble.gatts_indicate(conn_handle, self._handle)
 
     def _advertise(self, interval_us=100000):
         self._ble.gap_advertise(interval_us, adv_data=self._payload_1)
 
-    def _stop(self, interval_us = None):
+    def _stop(self, interval_us=None):
         self._ble.gap_advertise(interval_us, adv_data=self._payload_2)
 
-
-def periph(routedata,timeout):
+def periph(timeout, routedata = "test"):
     ble = bluetooth.BLE()
-    # gapname = manegement_01.nameinfo()
-
+    
+    # gapname = manegement_s1.nameinfo()
     jf_open = open('info/DN06.json', 'r')
     jf_load = json.load(jf_open)
     gapname = jf_load["device_number"]
     
-    ble.config(gap_name= str(gapname))
+    ble.config(gap_name=str(gapname))
     set_name = ble.config('gap_name')
-    print(set_name)    
+    print(set_name)
+    
     print(type(set_name))
-    
-    
-    name = set_name
-    name = set_name.decode('utf-8')
-    print(type(name))
-    
+    set_name = set_name.decode('utf-8')
+    print(type(set_name))
+    data = routedata + "_" + set_name
+    print(data)
+
     b = BLE(ble)
 
-    print("######")
-    print(type(routedata))
-    print(type(name))
-    
-    print("```````")
-    print(routedata)
-    
-    dt = routedata + '_' + name
-    dt.replace(" ",'')
-    dt.replace('\x00','')
-    
     i = 0
-    connect_count = 0
-
-    if b._check is False:
-        b._payload_1(jf_load["packet_name"])
-        while timeout > 0:
-            i = (i + 1) % 10
-            b.set_dev_name(dt, notify=i == 0, indicate=False)
-            print(".")
-            time.sleep_ms(1000)
-            timeout -= 1
-    print(dt)
-    return dt
     
-    if b._check is True:
-        connect_count += 1
+    b._payload_1(jf_load["packet_routeTS"])
+    print(timeout)
+    print(b._connect_count)
+    while timeout > 1 or b._connect_count is 0:
+        if b._check is False:
+            i = (i + 1) % 10
+            b.set_dev_name(data, notify=i == 0, indicate=False)
+            print(".")
+            utime.sleep_ms(1000)
+            timeout -=1
 
-    if b._check is False and connect_count is 1:
-       b._payload_2(jf_load["packet_name"])
-       i = (i + 1) % 10
-       b.set_dev_name(data, notify=i == 0, indicate=False)
-       print("アドバタイズ完了")
-
-
+    if timeout is 0 or b._connect_count is 1:
+    #if timeout is 0:
+        b._payload_3(jf_load["packet_routeTS"])
+        b.set_dev_name(data, notify=i == 0, indicate=False)
+        print("終了")
+        
 if __name__ == "__main__":
-    periph()
+    data = "test"
+    periph(data)
+
