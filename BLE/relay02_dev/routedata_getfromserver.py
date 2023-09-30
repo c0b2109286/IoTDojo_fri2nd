a79371c34c486b6b8dbafa07f60e7fe105349895
c0b21075 <85730292+c0b2107561@users.noreply.github.com>
2023-09-30 18:07:05 +0900

    Update routedata_getfromserver.py

--- BLE/relay02_dev/routedata_getfromserver.py
+++ BLE/relay02_dev/routedata_getfromserver.py
@@ -1,62 +1,53 @@
 import socket
-#import pickle
 import ubinascii
+import urequests
+import time
+import json
+import pickle
 
 def get():
-    host1 = '192.168.229.159' #自身のIPアドレス
-    port1 = 80
     
-    GET = True
-
-    socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket1.bind((host1, port1))
-    socket1.listen(1)
-
-    print('クライアントからの入力待ち状態')
-
-    # コネクションとアドレスを取得
-    connection, address = socket1.accept()
-    print('接続したクライアント情報:'  + str(address))
-
-    # 無限ループ　byeの入力でループを抜ける
-    recvline = ''
-    sendline = ''
-    num = 0
-    #while GET is True:
-
-        # クライアントからデータを受信
-    recvline = connection.recv(4096)
-        #if recvline == 'bye':
-        #    break
+    route = None
     
-    #getdata = pickle.loads(recvline)
-    print(type(recvline))
-    print(recvline)
-    if ')' in recvline:
-        gd = recvline.replace(b'\x80\x04\x95)\x00\x00\x00\x00\x00\x00\x00]\x94(]\x94\x8c\x07', b'')
-        gd = gd.replace(b'\x94a]\x94\x8c\r', b'')
-        gd = gd.replace(b'\x94ah\x01h\x03e.', b'')
-        #gd = gd.replace(b'_',b'')
-
-    if ',' in recvline:
-        gd = recvline.replace(b'\x80\x04\x95\x19\x00\x00\x00\x00\x00\x00\x00\x8c\x15',b'')
-        gd = gd.replace(b'\x94.',b'')
-        #gd = gd.replace(b'_',b'')
-        gd = gd.replace(b',',b'\n')
-    #getdata = int(ubinascii.unhexlify(gd), 'utf-8')
-
-    print(gd)
-    getdata = gd.decode('utf-8')
-
-    print('経路データ' + getdata)
-    print(type(getdata))
-        
-            
-    # クローズ
-    connection.close()
-    socket1.close()
-    print('サーバー側終了です')
-    return getdata
+    url = "http://192.168.193.229:5000/send_to_esp"
+    #urequests.head(url)
+    print("test")
+    
+    sendData = {"sign": "get"}
+    print(sendData)
+    header = {'Content-Type': 'application/json'}
+    
+    res = urequests.post(url, data=json.dumps(sendData),headers=header)
+    print("サーバからのステータスコード：", res.status_code)
+    #print(res)
+    resp = res.json()
+    
+    resp = resp["routedata"]
+    recv = bytes(resp, "utf-8")
+    recv = pickle.loads(recv)
+    print(type(recv))
+    
+    if b'\x80\x04]\x94.' in recv:
+        print("空のlistを受け取りました．")
+    else:
+        recv = recv.replace(b'\x80\x04\x95\x1d', b'')
+        recv = recv.replace(b'\x00\x00\x00\x00\x00\x00\x00]\x94(\x8c',b'')
+        recv = recv.replace(b'\t', b'')
+        recv = recv.replace(b'\x94\x8c', b'\n')
+        recv = recv.replace(b'\x94e.', b'')
+        recv = recv.replace(b'\x80\x04\x95)', b'')
+        recv = recv.replace(b'\x0b',b'')
+        recv = recv.replace(b'\x80\x04\x957', b'')
+        print(recv)
+        route = str(recv,'utf-8')
+        print(route)
+        print(type(route))
+    
+    print('終了です')
+    return route
 
 if __name__ == "__main__":
-    get()
+    data = get()
+    with open('data/routetabledata.txt','w',encoding='utf-8')as f:
+        f.write(data)
+        f.close()
