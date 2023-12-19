2b58d6d8b80569c01d6d3a0f8754b2b612c09f81
c0b21075 <85730292+c0b2107561@users.noreply.github.com>
2023-12-19 13:49:59 +0900

    Update manegement.py

--- BLE/relay02/manegement.py
+++ BLE/relay02/manegement.py
@@ -1,1 +1,42 @@
+import route_manegement
+import distance_manegement
+import ubinascii
+import utime
 
+
+def MG():
+    
+    Pmode_change = 0
+    Cmode_change = 0
+    fn = 'info/DN02.json'
+    
+    # 中継器1
+    utime.sleep(3)
+    mode_change = route_manegement.MGRoute(fn, Pmode_change, Cmode_change)
+    print("+++++++")
+    print(mode_change)
+    
+    utime.sleep(5)
+    
+    #中継器2
+    #mode_change = route_manegement.MGRoute(fn, Pmode_change, Cmode_change)
+    utime.sleep(40)
+    Pmode_change = 2
+    Cmode_change = 2
+    print(Pmode_change)
+    print(Cmode_change)
+    
+    utime.sleep(10)
+    
+    #距離送信
+    
+    utime.sleep(15)
+    print("distance")
+    
+    for i in range(2):
+        distance_manegement.MGDist(fn, Pmode_change, Cmode_change)
+        utime.sleep(15)
+
+if __name__ == "__main__":
+    print(ubinascii.hexlify('forsenser'))
+    MG()
